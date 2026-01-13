76e829880bc9660b41e0f723ed8e1d248a0fdc17
sohitkumar7 <sohit3165@gmail.com>
2026-01-13 22:00:09 +0530

    completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,53 +1,61 @@
 import React, { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
+
 import AuthLayout from "./components/auth/layout";
 import AuthLogin from "./pages/auth/login";
 import AuthRegister from "./pages/auth/register";
+
 import AdminLayout from "./components/admin-view/layout.jsx";
 import Adminproduct from "./pages/admin-view/product.jsx";
 import Admindashboard from "./pages/admin-view/dashboard.jsx";
 import Adminorder from "./pages/admin-view/order.jsx";
 import Adminfeatures from "./pages/admin-view/features.jsx";
+
 import Shoppingloyout from "./components/shopping-view/loyout.jsx";
-import Notindex from "./pages/not-found/index.jsx";
 import Shoppingaccount from "./pages/shopping-view/account.jsx";
 import Shoppinglisting from "./pages/shopping-view/listing.jsx";
 import Shoppinghome from "./pages/shopping-view/home.jsx";
 import Shoppingcheckout from "./pages/shopping-view/checkout.jsx";
-import Checkauth from "./components/commmon/check-auth.jsx";
-import Unauthpage from "./pages/unauth-age/index.jsx";
-import { Toaster } from 'react-hot-toast'
-import { useDispatch, useSelector } from "react-redux";
-import { checkAuth } from "./store/auth-slice";
 import PaypalReturnPage from "./pages/shopping-view/paypal-return.jsx";
 import PaymentSuccessPage from "./pages/shopping-view/payment-success.jsx";
 import SearchProducts from "./pages/shopping-view/search.jsx";
 
-function App() {
+import Notindex from "./pages/not-found/index.jsx";
+import Unauthpage from "./pages/unauth-age/index.jsx";
 
-  const {isAuthenticated,user,isLoading} = useSelector(state => state.auth)
-  
+import Checkauth from "./components/commmon/check-auth.jsx";
+import { Toaster } from "react-hot-toast";
+
+import { useDispatch, useSelector } from "react-redux";
+import { checkAuth } from "./store/auth-slice";
+
+function App() {
+  const { isAuthenticated, user, isLoading } = useSelector(
+    (state) => state.auth
+  );
+
   const dispatch = useDispatch();
 
-  useEffect(()=>{
+  useEffect(() => {
     dispatch(checkAuth());
-  },[dispatch])
+  }, [dispatch]);
 
-  if(isLoading) return <div>Loading....</div>
+  if (isLoading) return <div>Loading....</div>;
 
   return (
     <div className="flex-col overflow-hidden bg-white">
       <Routes>
- 
-        <Route path="/"  element={
-            <Checkauth isAutheticated={isAuthenticated} user={user}>
-            </Checkauth>
-          }/>
+        <Route
+          path="/"
+          element={
+            <Checkauth isAuthenticated={isAuthenticated} user={user} />
+          }
+        />
 
         <Route
           path="/auth"
           element={
-            <Checkauth isAutheticated={isAuthenticated} user={user}>
+            <Checkauth isAuthenticated={isAuthenticated} user={user}>
               <AuthLayout />
             </Checkauth>
           }
@@ -59,7 +67,7 @@ function App() {
         <Route
           path="/admin"
           element={
-            <Checkauth isAutheticated={isAuthenticated} user={user}>
+            <Checkauth isAuthenticated={isAuthenticated} user={user}>
               <AdminLayout />
             </Checkauth>
           }
@@ -71,9 +79,9 @@ function App() {
         </Route>
 
         <Route
-          path="/Shop"
+          path="/shop"
           element={
-            <Checkauth isAutheticated={isAuthenticated} user={user}>
+            <Checkauth isAuthenticated={isAuthenticated} user={user}>
               <Shoppingloyout />
             </Checkauth>
           }
@@ -82,20 +90,16 @@ function App() {
           <Route path="listing" element={<Shoppinglisting />} />
           <Route path="home" element={<Shoppinghome />} />
           <Route path="checkout" element={<Shoppingcheckout />} />
-          <Route path="paypal-return" element={<PaypalReturnPage/>} />
-          <Route path="payment-success" element={<PaymentSuccessPage/>} />
-          <Route path="search" element={<SearchProducts/>} />
-
+          <Route path="paypal-return" element={<PaypalReturnPage />} />
+          <Route path="payment-success" element={<PaymentSuccessPage />} />
+          <Route path="search" element={<SearchProducts />} />
         </Route>
 
-        <Route path="*" element={<Notindex />}>
-          {" "}
-        </Route>
-        <Route path="/unauth-page" element={<Unauthpage/>}/>
-
+        <Route path="/unauth-page" element={<Unauthpage />} />
+        <Route path="*" element={<Notindex />} />
       </Routes>
 
-          <Toaster/>
+      <Toaster />
     </div>
   );
 }
--- frontend/src/store/auth-slice/index.js
+++ frontend/src/store/auth-slice/index.js
@@ -3,103 +3,80 @@ import api from "@api";
 
 const initialState = {
   isAuthenticated: false,
-  isLoading: true,
+  isLoading: false,
   user: null,
 };
 
 export const registerUser = createAsyncThunk(
   "/auth/register",
-
   async (formData) => {
-    const response = await api.post("/api/auth/register", formData,{
-      withCredentials: true,
-    });
-
+    const response = await api.post("/api/auth/register", formData);
     return response.data;
   }
 );
 
 export const loginUser = createAsyncThunk(
   "/auth/login",
-
   async (formData) => {
-    const response = await api.post("/api/auth/login", formData,{
-      withCredentials: true,
-    });
-
+    const response = await api.post("/api/auth/login", formData);
     return response.data;
   }
 );
 
-export const checkAuth  = createAsyncThunk(
+export const checkAuth = createAsyncThunk(
   "/auth/check-auth",
-
-   async () => {
-    const response = await api.get(
-      "/api/auth/check-auth",
-      {
-        withCredentials: true,
-        headers: {
-          "Cache-Control":
-            "no-store, no-cache, must-revalidate, proxy-revalidate",
-        },
-      }
-    );
-
+  async () => {
+    const response = await api.get("/api/auth/check-auth", {
+      headers: {
+        "Cache-Control": "no-store",
+      },
+    });
     return response.data;
   }
 );
 
 export const logoutUser = createAsyncThunk(
   "/auth/logout",
-
-  async (formData) => {
-    const response = await api.post("/api/auth/logout",{
-      withCredentials: true,
-    });
-
+  async () => {
+    const response = await api.post("/api/auth/logout");
     return response.data;
   }
 );
 
-
 const authSlice = createSlice({
   name: "auth",
   initialState,
-  reducers: {
-    setUser: (state, action) => {},
-  },
+  reducers: {},
 
   extraReducers: (builder) => {
     builder
+
       .addCase(registerUser.pending, (state) => {
         state.isLoading = true;
       })
-      .addCase(registerUser.fulfilled, (state, action) => {
+      .addCase(registerUser.fulfilled, (state) => {
         state.isLoading = false;
         state.user = null;
         state.isAuthenticated = false;
       })
-      .addCase(registerUser.rejected, (state, action) => {
-        state.isLoading = true;
-        state.user = null;
-        state.isAuthenticated = false;
+      .addCase(registerUser.rejected, (state) => {
+        state.isLoading = false;
       })
+
       .addCase(loginUser.pending, (state) => {
         state.isLoading = true;
       })
       .addCase(loginUser.fulfilled, (state, action) => {
-        console.log(action);
         state.isLoading = false;
-        state.user = !action.payload.success ? null : action.payload.User;
-        state.isAuthenticated = !action.payload.success ? false : true;
-        window.location.reload();
+        state.user = action.payload.success ? action.payload.User : null;
+        state.isAuthenticated = action.payload.success;
       })
-      .addCase(loginUser.rejected, (state, action) => {
+      .addCase(loginUser.rejected, (state) => {
         state.isLoading = false;
         state.user = null;
         state.isAuthenticated = false;
       })
+
       .addCase(checkAuth.pending, (state) => {
         state.isLoading = true;
       })
@@ -108,17 +85,17 @@ const authSlice = createSlice({
         state.user = action.payload.success ? action.payload.user : null;
         state.isAuthenticated = action.payload.success;
       })
-      .addCase(checkAuth.rejected, (state, action) => {
+      .addCase(checkAuth.rejected, (state) => {
         state.isLoading = false;
         state.user = null;
         state.isAuthenticated = false;
-      }).addCase(logoutUser.fulfilled, (state, action) => {
-        state.isLoading = false;
-        state.user =  null;
-        state.isAuthenticated = false;
       })
+
+      .addCase(logoutUser.fulfilled, (state) => {
+        state.user = null;
+        state.isAuthenticated = false;
+      });
   },
 });
 
-export const { setUser } = authSlice.actions;
 export default authSlice.reducer;
